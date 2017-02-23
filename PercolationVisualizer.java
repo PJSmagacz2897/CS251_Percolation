c30d22f1d53438117f7085b3f20d921d02b3be29
Jimmy Smagacz <jsmagac@purdue.edu>
2017-02-23 17:20:22 -0500

    Create PercolationVisualizer.java

---
+++ PercolationVisualizer.java
@@ -0,0 +1,25 @@
+import java.io.*;
+public class PercolationVisualizer {
+
+       public static void main(String[] args) {
+        int n = StdIn.readInt();
+        Percolation p = new Percolation(n);
+        Out outputFile = "visualMatrix.txt"
+        String output = n + "\n\n";
+        while (!StdIn.isEmpty()) {
+            int x = StdIn.readInt();
+            int y = StdIn.readInt();
+            p.open(x,y);
+            for(int i = p.side - 1; i >= 0; i--) {
+                for(int j = 0; j < p.side; j++) {
+                 	if(p.uf.connected(p.top, p.side*i + j)) output += "2 ";
+                 	else output += p.grid[i][j] + " ";
+                }
+                output += "\n";
+            }
+            output += "\n";
+        }
+        StdOut.print(output);
+        outputFile.println(output);
+    }
+}
